5e134a1c35889f4c3efff718984b996905d8149b
Kristián Řehoř <kmancz.kiki@gmail.com>
2022-06-22 18:21:28 +0200

    Comment styling

--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -32,6 +32,33 @@
                     <x-like-button :post="$post" />
                 </div>
             </div>
+            <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg mt-5 mx-10">
+                <form class="p-6 bg-white border-b border-gray-200">
+                    <textarea name="" id="" class="resize-none w-full h-50 border border-solid rounded-lg" placeholder="Comment..."></textarea>
+                    <div class="flex justify-end">
+                        <button type="submit" class="inline-block rounded-lg border-solid border-2 border-black hover:bg-slate-400 py-1 px-3">Submit</button>
+                    </div>
+                </form>
+            </div>
+            <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg mt-5 mx-10">
+                <div class="p-6 bg-white border-b border-gray-200">
+                    <div class=" font-bold">Kman:</div>
+                    <div>fdsafdsafdas</div>
+                </div>
+                <div class="flex justify-end mr-5 my-2">
+                    <a href="" class="inline-block rounded-lg bg-blue-500 text-white hover:bg-blue-700 py-1 px-3">Edit</a>
+                    <form action="" method="POST" class="ml-2">
+                        @csrf
+                        @method("DELETE")
+                        <button type="submit" class="inline-block rounded-lg bg-red-500 text-white hover:bg-red-700 py-1 px-3">Delete</button>
+                    </form>
+                </div>
+            </div>
+            <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg mt-5 mx-10">
+                <div class="p-6 bg-white border-b border-gray-200">
+                    <div class=" font-bold">Test User:</div>
+                    <div>fdsfadsafdsa</div>
+                </div>
+            </div>
         </div>
-    </div>
 </x-app-layout>
