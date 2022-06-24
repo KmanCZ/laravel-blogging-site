b19fd404be4f79d68b2caf02f9e693ba31db5f57
Kristián Řehoř <kmancz.kiki@gmail.com>
2022-06-24 15:30:24 +0200

    default profile picture

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -16,10 +16,11 @@ return new class extends Migration
         Schema::create('users', function (Blueprint $table) {
             $table->id();
             $table->string('name');
-            $table->string('username');
+            $table->string('username')->unique();
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->string("profile_picture")->default("default/profile_picture.png");
             $table->rememberToken();
             $table->timestamps();
         });
