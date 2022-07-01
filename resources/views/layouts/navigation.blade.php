3955d9563dd8e22918c42320e9bbb1ba1f939aa3
Kristián Řehoř <kmancz.kiki@gmail.com>
2022-07-01 19:28:40 +0200

    Secure navigation asset

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -38,7 +38,7 @@
                 <x-dropdown align="right" width="48">
                     <x-slot name="trigger">
                         <button class="flex items-center text-sm font-medium text-gray-500 hover:text-gray-700 hover:border-gray-300 focus:outline-none focus:text-gray-700 focus:border-gray-300 transition duration-150 ease-in-out">
-                            <img class="w-10 h-10 mx-2 rounded-full" src="{{asset("storage/". auth()->user()->profile_picture)}}" alt="profile picture">
+                            <img class="w-10 h-10 mx-2 rounded-full" src="{{secure_asset("storage/". auth()->user()->profile_picture}}" alt="profile picture">
                             <div>{{ Auth::user()->name }}</div>
 
                             <div class="ml-1">
